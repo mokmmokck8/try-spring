9347ad54d820fe386041aa58209a03c5b1b6e3a3
Leo Mok <leomok.smoothbiz.io>
2025-04-19 17:01:51 +0800

    feat: update to course 148

--- src/main/java/com/luv2code/sprintboot/demo/mycoolapp/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/sprintboot/demo/mycoolapp/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.luv2code.sprintboot.demo.mycoolapp.dao;
 
 import com.luv2code.sprintboot.demo.mycoolapp.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 // Same functionality as EmployeeDAO and EmployeeDAOImpl, use JpaRepository
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
--- src/main/java/com/luv2code/sprintboot/demo/mycoolapp/security/SecurityConfig.java
+++ src/main/java/com/luv2code/sprintboot/demo/mycoolapp/security/SecurityConfig.java
@@ -2,15 +2,28 @@ package com.luv2code.sprintboot.demo.mycoolapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails john = User.builder().username("john").password("{noop}test123").roles("EMPLOYEE").build();
+        UserDetails mary = User.builder().username("mary").password("{noop}test123").roles("EMPLOYEE", "MANAGER").build();
+        UserDetails susan = User.builder().username("susan").password("{noop}test123").roles("EMPLOYEE", "MANAGER", "ADMIN").build();
+
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -19,8 +32,12 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().authenticated()
-                );
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+                ).httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
